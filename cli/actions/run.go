c0661287be2b43b4af30278e73eea8e06451a9a8
Aaron Schlesinger <arschles@gmail.com>
2017-12-08 01:02:50 -0600

    building more of the CLI

---
+++ cli/actions/run.go
@@ -0,0 +1,111 @@
+package actions
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	// "path/filepath"
+
+	"github.com/spf13/cobra"
+	// "github.com/kubehelium/helium/cli/util/docker"
+	log "github.com/kubehelium/helium/pkg/log/human"
+	"github.com/kubehelium/helium/pkg/runtime"
+)
+
+// Run is the CLI handler for 'he run'
+func Run() *cobra.Command {
+	return &cobra.Command{
+		Use:   "run",
+		Short: "Execute an individual event on the helium build script",
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("Usage: he run <event>")
+			}
+			event := args[0]
+			// event := c.String("target")
+			// dockerClient := docker.ClientOrDie()
+			log.Info("build")
+			srv := runtime.NewServer(8080)
+			srvErrCh := make(chan error)
+			go func() {
+				log.Debug("Running the event handler server")
+				srvErrCh <- srv.Run()
+			}()
+
+			imgStr := "helium-runner:v0.0.1"
+			// img, err := docker.ParseImageFromName(imgStr)
+			// if err != nil {
+			// log.Err("error parsing docker image %s (%s)", imgStr, err)
+			// os.Exit(1)
+			// }
+
+			// rmContainerCh := make(chan func())
+			// stdOutCh := make(chan docker.Log)
+			// stdErrCh := make(chan docker.Log)
+			// exitCodeCh := make(chan int)
+			// errCh := make(chan error)
+
+			// TODO: make configurable
+			wd, err := os.Getwd()
+			if err != nil {
+				return fmt.Errorf("error getting current working dir")
+			}
+			heliumDir := fmt.Sprintf("%s/.helium", wd)
+			log.Info("using script dir %s", heliumDir)
+			cmd := exec.Command(
+				"docker",
+				"run",
+				"--rm",
+				"-v",
+				fmt.Sprintf("%s:/helium", heliumDir),
+				"-e",
+				"HELIUM_SCRIPTS_DIR=/helium",
+				"-e",
+				"HELIUM_TARGET_DIR=/home/src/dist/src",
+				"-e",
+				fmt.Sprintf("HELIUM_EVENT=%s", event),
+				imgStr,
+			)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			log.Debug(strings.Join(cmd.Args, " "))
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("error running command (%s)", err)
+			}
+			// go docker.Run(
+			// 	dockerClient,
+			// 	img,
+			// 	"helium-runner",
+
+			// 	docker.ContainerGopath(cfg.Build.Gopath, paths.PackageName),
+			// 	fmt.Sprintf("go build -o %s .", binaryName),
+			// 	cfg.Build.Env,
+			// 	rmContainerCh,
+			// 	stdOutCh,
+			// 	stdErrCh,
+			// 	exitCodeCh,
+			// 	errCh,
+			// )
+
+			// for {
+			// 	select {
+			// 	case rmContainerFn := <-rmContainerCh:
+			// 		defer rmContainerFn()
+			// 	case l := <-stdOutCh:
+			// 		log.Info("%s", l)
+			// 	case l := <-stdErrCh:
+			// 		log.Warn("%s", l)
+			// 	case err := <-errCh:
+			// 		log.Err("%s", err)
+			// 		return
+			// 	case i := <-exitCodeCh:
+			// 		log.Info("exited with code %d", i)
+			// 		return
+			// 	}
+			// }
+			return nil
+		},
+	}
+
+}
--- cli/main.go
+++ cli/main.go
@@ -1,75 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/codegangsta/cli"
 	"github.com/kubehelium/helium/cli/actions"
-	log "github.com/kubehelium/helium/pkg/log/human"
+	// log "github.com/kubehelium/helium/pkg/log/human"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "helium"
-	app.Usage = "helium is your one-stop shop for building and deploying Kubernetes apps"
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  actions.FlagDebug,
-			Usage: "Enable verbose debugging output",
-		},
-		cli.StringFlag{
-			Name:  actions.FlagConfigDir,
-			Usage: "The location of the config directory",
+	cmd := &cobra.Command{
+		Use:          "he",
+		Short:        "Your one stop shop for building and deploying Kubernetes apps",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// TODO: version.
+			// see https://github.com/Azure/service-catalog-cli/blob/master/cmd/svcat/main.go#L34
+			fmt.Print(cmd.UsageString())
+			return nil
 		},
 	}
+	cmd.AddCommand(actions.Run())
+	// TODO:
+	//
+	// app.Flags = []cli.Flag{
+	// 	cli.BoolFlag{
+	// 		Name:  actions.FlagDebug,
+	// 		Usage: "Enable verbose debugging output",
+	// 	},
+	// 	cli.StringFlag{
+	// 		Name:  actions.FlagConfigDir,
+	// 		Usage: "The location of the config directory",
+	// 	},
+	// }
 
-	app.Commands = []cli.Command{
-		{
-			Name:        "build",
-			Aliases:     []string{"b"},
-			Usage:       "Build your project",
-			Description: "This command runs the equivalent of 'go build -o $CURRENT_DIR_NAME'",
-			Action:      actions.Build,
-		},
-		// {
-		// 	Name:        "docker",
-		// 	Aliases:     []string{"d"},
-		// 	Description: "Docker related actions",
-		// 	Subcommands: []cli.Command{
-		// 		{
-		// 			Name:        "build",
-		// 			Aliases:     []string{"db"},
-		// 			Usage:       "Build a Docker image for your project",
-		// 			Description: "This command runs the equivalent of 'docker build -t $IMG_NAME $DOCKERFILE_DIR'",
-		// 			Action:      actions.DockerBuild,
-		// 		},
-		// 		{
-		// 			Name:        "doc-push",
-		// 			Aliases:     []string{"dp"},
-		// 			Usage:       "Push the Docker image for your project",
-		// 			Description: "This command runs the equivalent of 'docker push $IMG_NAME'",
-		// 			Action:      actions.DockerPush,
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	Name:    "custom",
-		// 	Aliases: []string{"c"},
-		// 	Flags: []cli.Flag{
-		// 		cli.BoolFlag{
-		// 			Name: actions.ListCustomFlag,
-		// 		},
-		// 	},
-		// 	Usage:       "Run a custom target",
-		// 	Description: "Run a custom build target.",
-		// 	Action:      actions.Custom,
-		// },
+	// app.Before = func(c *cli.Context) error {
+	// 	log.IsDebugging = c.Bool(actions.FlagDebug)
+	// 	return nil
+	// }
+
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
 	}
-
-	app.Before = func(c *cli.Context) error {
-		log.IsDebugging = c.Bool(actions.FlagDebug)
-		return nil
-	}
-
-	app.Run(os.Args)
 }
